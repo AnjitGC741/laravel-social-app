9174df9bd9f9d746e5b629ddec9ef95eb31954e0
ANjitGC741 <anzit741@gmail.com>
2022-11-22 12:21:46 +0545

    first commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Article App</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+        <nav>
+                <ul>
+                    <li>Dashboard</li>
+                    <li>login</li>
+                </ul>
+            </nav>
+            <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+            @yield('content')
+</body>
+</html>
